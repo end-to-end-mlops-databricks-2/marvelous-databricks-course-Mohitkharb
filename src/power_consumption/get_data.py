60704fa1ab9f2e274f2b547c96f837f5956cefe8
Mohit Kumar <mohit.kumar@eneco.com>
2025-02-05 15:54:01 +0100

    ci: pre-commit-linting

--- src/power_consumption/get_data.py
+++ src/power_consumption/get_data.py
@@ -1,16 +1,16 @@
-# Databricks notebook source 
+# Databricks notebook source
 
-from ucimlrepo import fetch_ucirepo 
-  
-# fetch dataset 
-power_consumption_of_tetouan_city = fetch_ucirepo(id=849) 
-  
-# data (as pandas dataframes) 
-X = power_consumption_of_tetouan_city.data.features 
-y = power_consumption_of_tetouan_city.data.targets 
-  
-# metadata 
-print(power_consumption_of_tetouan_city.metadata) 
-  
-# variable information 
-print(power_consumption_of_tetouan_city.variables) 
+from ucimlrepo import fetch_ucirepo
+
+# fetch dataset
+power_consumption_of_tetouan_city = fetch_ucirepo(id=849)
+
+# data (as pandas dataframes)
+X = power_consumption_of_tetouan_city.data.features
+y = power_consumption_of_tetouan_city.data.targets
+
+# metadata
+print(power_consumption_of_tetouan_city.metadata)
+
+# variable information
+print(power_consumption_of_tetouan_city.variables)
