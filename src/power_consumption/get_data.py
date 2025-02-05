4dee1a484fd8237b1beaa219f78b6f79ab08c0eb
Mohit Kumar <mohit.kumar@eneco.com>
2025-02-05 16:10:11 +0100

    refactor : create method

--- src/power_consumption/get_data.py
+++ src/power_consumption/get_data.py
@@ -1,16 +1,12 @@
-# Databricks notebook source
 
 from ucimlrepo import fetch_ucirepo
 
-# fetch dataset
-power_consumption_of_tetouan_city = fetch_ucirepo(id=849)
 
-# data (as pandas dataframes)
-X = power_consumption_of_tetouan_city.data.features
-y = power_consumption_of_tetouan_city.data.targets
-
-# metadata
-print(power_consumption_of_tetouan_city.metadata)
-
-# variable information
-print(power_consumption_of_tetouan_city.variables)
+def get_data(num_records: int = 1000):
+    # fetch dataset
+    power_consumption_of_tetouan_city = fetch_ucirepo(id=849)
+    
+    # data (as pandas dataframes)
+    X = power_consumption_of_tetouan_city.data.features
+    y = power_consumption_of_tetouan_city.data.targets
+    return X, y
