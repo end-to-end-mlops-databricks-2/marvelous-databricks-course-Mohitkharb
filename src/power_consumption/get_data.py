3c9942842b804b4e6466686edb473a502dd234c3
Mohit Kumar <mohit.kumar@eneco.com>
2025-02-05 16:12:35 +0100

    fix: get-data-for-unit-id

--- src/power_consumption/get_data.py
+++ src/power_consumption/get_data.py
@@ -1,10 +1,10 @@
 
 from ucimlrepo import fetch_ucirepo
+import pandas as pd
 
-
-def get_data(num_records: int = 1000):
+def get_data(id: int) -> tuple[pd.DataFrame, pd.DataFrame]:
     # fetch dataset
-    power_consumption_of_tetouan_city = fetch_ucirepo(id=849)
+    power_consumption_of_tetouan_city = fetch_ucirepo(id=id)
     
     # data (as pandas dataframes)
     X = power_consumption_of_tetouan_city.data.features
