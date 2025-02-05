76397f6773fab50089343e87933241d15bbbac04
Mohit Kumar <mohit.kumar@eneco.com>
2025-02-05 16:14:28 +0100

    ci: pre-commit-fixes

--- src/power_consumption/get_data.py
+++ src/power_consumption/get_data.py
@@ -1,11 +1,11 @@
-
-from ucimlrepo import fetch_ucirepo
 import pandas as pd
+from ucimlrepo import fetch_ucirepo
+
 
 def get_data(id: int) -> tuple[pd.DataFrame, pd.DataFrame]:
     # fetch dataset
     power_consumption_of_tetouan_city = fetch_ucirepo(id=id)
-    
+
     # data (as pandas dataframes)
     X = power_consumption_of_tetouan_city.data.features
     y = power_consumption_of_tetouan_city.data.targets
